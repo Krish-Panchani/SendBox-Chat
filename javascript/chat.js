76367ced3905db6ae9d743419324be481d408f20
Krish-Panchani <krishpanchani1346@gmail.com>
2023-11-08 11:14:32 +0530

    Added missing lines

--- javascript/chat.js
+++ javascript/chat.js
@@ -10,17 +10,10 @@ form.onsubmit = (e)=>{
 
 inputField.focus();
 chatBox.focus();
-// inputField.onkeyup = ()=>{
-//     if(inputField.value != ""){
-//         sendBtn.classList.add("active");
-//     }else{
-//         sendBtn.classList.remove("active");
-//     }
-// }
 
 sendBtn.onclick = ()=>{
     let xhr = new XMLHttpRequest();
-    // xhr.open("POST", "php/insert-chat.php", true);
+    xhr.open("POST", "php/insert-chat.php", true);
     xhr.onload = ()=>{
       if(xhr.readyState === XMLHttpRequest.DONE){
           if(xhr.status === 200){
@@ -49,7 +42,7 @@ chatBox.ontouchmove = ()=>{
 
 setInterval(() =>{
     let xhr = new XMLHttpRequest();
-    // xhr.open("POST", "php/get-chat.php", true);
+    xhr.open("POST", "php/get-chat.php", true);
     xhr.onload = ()=>{
       if(xhr.readyState === XMLHttpRequest.DONE){
           if(xhr.status === 200){
